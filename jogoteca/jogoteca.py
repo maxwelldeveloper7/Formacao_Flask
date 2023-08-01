effc6f4f026ff513d6bf8747b9464ea916377e9f
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-08-01 17:12:00 -0300

    aula 03

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console) -> None:
@@ -6,14 +6,32 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('Gog of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)# faz referencia para o proprio arquivo garantindo de que vai rodar a aplicação
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('Gog of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
-    lista = [jogo1, jogo2, jogo3]
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos = lista)
 
-app.run(host='0.0.0.0', port=8080)# faz executar a aplicação
+
+@app.route('/novo')
+
+def novo():
+    return render_template('novo.html', titulo = 'Novo Jogo')
+
+
+@app.route('/criar', methods=['POST',])
+
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']    
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo = "Jogos", jogos = lista)
+
+app.run(host='0.0.0.0', port=8080, debug=True)# faz executar a aplicação
