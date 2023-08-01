73da82c5e7a30ddaf091106c4a111061621f84ba
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-08-01 16:03:41 -0300

    aula 02

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console) -> None:
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 app = Flask(__name__)# faz referencia para o proprio arquivo garantindo de que vai rodar a aplicação
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('Gog of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Jogos', jogos = lista)
 
 app.run(host='0.0.0.0', port=8080)# faz executar a aplicação
