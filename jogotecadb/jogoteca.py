e5e59697ac4ad719c8b05d10c51438f7f2f998a7
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-08-11 12:17:37 -0300

    aula 01

--- jogotecadb/jogoteca.py
+++ jogotecadb/jogoteca.py
@@ -1,35 +1,41 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-class Jogo:
-    def __init__(self, nome, categoria, console) -> None:
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
-
-jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-jogo2 = Jogo('Gog of War', 'Rack n Slash', 'PS2')
-jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
-lista = [jogo1, jogo2, jogo3]
-
-class Usuario:
-    def __init__(self, nome, nickname, senha):
-        self.nome = nome
-        self.nickname = nickname
-        self.senha = senha
-
-usuario1 = Usuario("Bruno Divino", "BD", "alohomora")
-usuario2 = Usuario("Maxwelll", "Max", "1")
-usuario3 = Usuario("Camila Ferreira", "Mila", "2")
-
-usuarios = { usuario1.nickname : usuario1,
-             usuario2.nickname : usuario2,
-             usuario3.nickname : usuario3 }
 
 app = Flask(__name__)# faz referencia para o proprio arquivo garantindo de que vai rodar a aplicação
 app.secret_key = 'alura' # camada de criptografia
 
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    '{SGDB}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGDB = 'mysql+mysqlconnector',
+        usuario = 'dev',
+        senha = '@Developer7', password no mysql com @
+        servidor = 'localhost',
+        database = 'jogoteca'
+    )
+
+db = SQLAlchemy(app)
+
+class Jogos(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(50), nullable = False)
+    categoria = db.Column(db.String(40), nullable = False)
+    console = db.Column(db.String(20), nullable = False)
+    
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+class Usuarios(db.Model):
+    nickname = db.Column(db.String(8), primary_key=True)
+    nome = db.Column(db.String(20), nullable = False)
+    senha = db.Column(db.String(100), nullable = False)
+    
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
 @app.route('/')
 def index():
+    lista = Jogos.query.order_by(Jogos.id)
     return render_template('lista.html', titulo='Jogos', jogos = lista)
 
 
