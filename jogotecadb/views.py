0c0857ed6ca5c99aa77cdbb9ec9b348baffdb0d2
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-09-01 12:34:36 -0300

    aula 3.1

--- jogotecadb/views.py
+++ jogotecadb/views.py
@@ -31,7 +31,11 @@ def criar():
     
     novo_jogo = Jogos(nome = nome, categoria = categoria, console = console)
     db.session.add(novo_jogo)
-    db.session.commit()        
+    db.session.commit()
+    
+    arquivo = request.files['arquivo']
+    print(arquivo)
+    arquivo.save(f'uploads/{arquivo.filename}')
     
     return redirect(url_for('index'))
 
