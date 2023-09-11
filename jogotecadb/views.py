79a80b32870dafa9cc824d47f5151b931aae1553
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-09-11 08:57:58 -0300

    aula 4.2

--- jogotecadb/views.py
+++ jogotecadb/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
 from jogoteca import app, db
-from models import Jogos, Usuarios 
+from models import Jogos, Usuarios
+from helpers import recupera_imagem
+
 
 @app.route('/')
 def index():
@@ -45,7 +47,8 @@ def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login',proxima = url_for('editar')))
     jogo = Jogos.query.filter_by(id = id).first()
-    return render_template('editar.html', titulo = 'Editando Jogo', jogo = jogo)
+    capa_jogo = recupera_imagem(id)
+    return render_template('editar.html', titulo = 'Editando Jogo', jogo = jogo, capa_jogo=capa_jogo)
 
 @app.route('/atualizar', methods=['POST',]) 
 
@@ -56,6 +59,10 @@ def atualizar():
     jogo.console = request.form['console']
     db.session.add(jogo)
     db.session.commit()
+    
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    arquivo.save(f'{upload_path}/capa{jogo.id}.jpg')
     
     return redirect(url_for('index'))
 
