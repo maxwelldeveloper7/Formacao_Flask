f2ae6841a5c11986fc4578e7d3c0a47f3ce4b1ee
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-09-11 10:46:25 -0300

    fim aula 4

--- jogotecadb/helpers.py
+++ jogotecadb/helpers.py
@@ -1,8 +1,13 @@
-from jogoteca import app, db
+from jogoteca import app
 import os
 
 def recupera_imagem(id):
     for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
-        if f'capa{id}.jpg' == nome_arquivo:
+        if f'capa{id}' in nome_arquivo:
             return nome_arquivo
     return 'capa_padrao.jpg'
+
+def deleta_arquivo(id):
+    arquivo = recupera_imagem(id)
+    if arquivo != 'capa_padrao.jpg':
+        os.remove(os.path.join(app.config['UPLOAD_PATH'], arquivo))
